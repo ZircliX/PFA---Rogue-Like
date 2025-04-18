c6d0f65cb54b65a50456ba2f3ac53c7e86214a36
ZircliX <lolofy71@gmail.com>
2025-04-18 11:47:57 +0200

    Fix Camera Gravity Rotation + arms

--- Assets/Scripts/Player/HandsFollowCamera.cs
+++ Assets/Scripts/Player/HandsFollowCamera.cs
@@ -4,7 +4,8 @@ namespace DeadLink.Player
 {
     public class HandsFollowCamera : MonoBehaviour
     {
-        [SerializeField] private Transform cameraTransform;
+        [SerializeField] private Transform cameraRotations;
+        [SerializeField] private Transform cameraRoot;
         [SerializeField] private Transform headTransform;
         [SerializeField] private float armOffset = -0.25f;
         [SerializeField] private bool followPitch = true;
@@ -18,20 +19,20 @@ namespace DeadLink.Player
         private void MoveArms()
         {
             Vector3 pos = headTransform.position;
-            pos -= -cameraTransform.up * armOffset;
+            pos -= -cameraRotations.up * armOffset;
 
             transform.position = Vector3.Lerp(transform.position, pos, 40f * Time.deltaTime);
         }
 
         private void RotateArms()
         {
-            Vector3 euler = cameraTransform.eulerAngles;
+            Vector3 euler = cameraRotations.eulerAngles;
         
             transform.rotation = Quaternion.Slerp(
                 transform.rotation,
                 Quaternion.Euler(followPitch ? euler.x : 0f,
                     euler.y,
-                    0f),
+                    cameraRoot.eulerAngles.z),
                 40f * Time.deltaTime
             );
         }
