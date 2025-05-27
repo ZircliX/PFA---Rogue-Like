5b082d363ca3988c7d65bff0ac9f2646d83255db
ttakkeee <157479951+ttakkeee@users.noreply.github.com>
2025-05-27 18:25:47 +0200

    VoiceLine System

---
+++ Assets/Scripts/VoiceLines/VoiceLinesManager.cs
@@ -0,0 +1,37 @@
+using FMODUnity;
+using LTX.Singletons;
+using TMPro;
+using UnityEngine;
+
+namespace DeadLink.VoiceLines
+{
+    public class VoiceLinesManager : MonoSingleton<VoiceLinesManager>
+    {
+        [field: SerializeField] public EventReference[] AudioEvents { get; private set; }
+        [field: SerializeField] public EventReference PlayerHitEvent { get; private set; }
+        [field: SerializeField] public VoiceLineData[] VoiceLineData { get; private set; }
+
+        [SerializeField] private Transform player;
+        [SerializeField] private TMP_Text dialogueText;
+
+        private bool playerHit;
+        
+        public void PlayVoiceLine(int voiceLinesIndex)
+        {
+            AudioManager.Global.PlayOneShot(AudioEvents[voiceLinesIndex], player.position);
+        }
+
+        public void PlayVoiceLineWithDialogue(int voiceLinesIndex)
+        {
+            dialogueText.text = VoiceLineData[voiceLinesIndex].Dialogue;
+            PlayVoiceLine(voiceLinesIndex);
+        }
+
+        public void PlayerHit()
+        {
+            if (playerHit) return;
+            playerHit = true;
+            AudioManager.Global.PlayOneShot(PlayerHitEvent, player.position);
+        }
+    }
+}
