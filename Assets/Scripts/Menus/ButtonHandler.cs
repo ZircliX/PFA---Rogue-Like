aa2c8b3d3e3f81cacca17eb6b300df2368bc08ee
Dydou_irl <MegaFight63@laposte.net>
2025-05-29 22:23:26 +0200

    Main menu + shader + impletation + ct long +
    (quelque truc a finir dans les options

---
+++ Assets/Scripts/Menus/ButtonHandler.cs
@@ -0,0 +1,75 @@
+using System;
+using KBCore.Refs;
+using RogueLike;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace DeadLink.Menus
+{
+    public class ButtonHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [Header("References")]
+        [field: SerializeField, Self] public Image OriginalImage { get; private set; } 
+        [field: SerializeField] public Sprite HighlightedImage { get; private set; } 
+        [field: SerializeField] public Material Material { get; private set; } 
+        [field: SerializeField] public TextMeshProUGUI Text { get; private set; } 
+        [field: SerializeField] public Color OriginalColor { get; private set; } 
+        [field: SerializeField] public Color Color { get; private set; }
+        
+        private Sprite _image;
+
+        private void OnValidate()
+        {
+            this.ValidateRefs();
+        }
+
+        private void Awake()
+        {
+            _image = OriginalImage.sprite;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            AudioManager.Global.PlayOneShot(GameMetrics.Global.FMOD_UIHover, eventData.position);
+            if (OriginalImage != null)
+            {
+                OriginalImage.material = Material;
+            }
+
+            if (HighlightedImage != null)
+            {
+                OriginalImage.sprite = HighlightedImage;
+            }
+
+            if (Text != null)
+            {
+                Text.color = Color;
+            }
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+
+            if (OriginalImage != null)
+            {
+                OriginalImage.material = null;  
+            }
+            
+            if (HighlightedImage != null)
+            {
+                OriginalImage.sprite = _image;
+            }
+            
+            if (Text != null)
+            {
+                Text.color = OriginalColor;
+            }
+        }
+        
+        
+        
+        
+    }
+}
