3172e74700f762abe9ca2b8ba98c3c6127ba7b8f
ZircliX <lolofy71@gmail.com>
2025-06-03 14:59:20 +0200

    Sauvetage

--- Assets/Scripts/Entities/Enemies/Implementation/Spider.cs
+++ Assets/Scripts/Entities/Enemies/Implementation/Spider.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using RogueLike;
 using UnityEngine;
 
@@ -5,25 +6,35 @@ namespace DeadLink.Entities.Enemies
 {
     public class Spider : MeleeEnemy
     {
+        private Vector3 direction;
+        private GameObject objectToHit;
+        
         protected override void Shoot()
         {
             if (CurrentWeapon != null && CurrentWeapon.CurrentReloadTime >= CurrentWeapon.WeaponData.ReloadTime)
             {
-                GameObject objectToHit = null;
-                Vector3 direction = player.transform.position - transform.position;
+                objectToHit = null;
+                direction = player.transform.position - transform.position;
                 Debug.DrawRay(BulletSpawnPoint.position, direction * 50, Color.red);
 
                 if (Physics.Raycast(transform.position, direction, out RaycastHit hit, 500, GameMetrics.Global.BulletRayCast))
                 {
                     objectToHit = hit.collider.gameObject;
                 }
-                CurrentWeapon.Fire(this, direction, objectToHit);
                 StartCoroutine(Die());
             }
             else
             {
                 //Debug.LogError($"No equipped weapon for {gameObject.name}");
             }
+        }
+
+        public override IEnumerator Die()
+        {
+            yield return new WaitForSeconds(1f);
+            CurrentWeapon.Fire(this, direction, objectToHit);
+            AudioManager.Global.PlayOneShot(GameMetrics.Global.FMOD_EnemyExplode, transform.position);
+            yield return base.Die();
         }
     }
 }
